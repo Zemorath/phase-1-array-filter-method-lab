358750462795a67e2776d03927e005b783061f8c
Zemorath <trentward10@gmail.com>
2023-05-12 23:35:47 -0500

    Filter Method Complete

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution here
+
+function findMatching(drivers, string) {
+    let match = drivers.filter(driver => {
+        if (driver.toLowerCase() === string.toLowerCase()) {
+            return driver;
+        }
+    })
+
+    if (match) {
+        return match;
+    }
+}
+
+function fuzzyMatch(drivers, string) {
+    let match = drivers.filter(driver => {
+        if (driver.startsWith(string)) {
+            return driver;
+        }
+    })
+
+    if (match) {
+        return match;
+    }
+}
+
+function matchName(drivers, string) {
+    let match = drivers.filter(driver => {
+        if (driver.name === string) {
+            return driver;
+        }
+    })
+
+    if (match) {
+        return match;
+    }
+}
